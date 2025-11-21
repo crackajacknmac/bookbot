0185957cd76290887ea47461e1fc6902d7f522ad
crackajacknmac <crackajacknmac@gmail.com>
2025-11-21 11:50:54 -0800

    E: Refactored function

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from stats import book_word_count
 
 def get_book_text(filepath):
-	with open(filepath) as f:
-		text = f.read()
-		return text
+	with open("books/frankenstein.txt") as f:
+		book_string = f.read()
+		return book_string
 
 def main():
+	text = get_book_text("books/frankenstein.txt")
 	num_words = book_word_count(text)
 	print(f"Found {num_words} total words")
 
