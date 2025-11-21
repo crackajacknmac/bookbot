8260552ebb4d067274e5f85c9173e47aa6050965
crackajacknmac <crackajacknmac@gmail.com>
2025-11-21 07:33:04 -0800

    A: add .gitignore; wrote main.py block

---
+++ main.py
@@ -0,0 +1,13 @@
+"""Okay, so I need to write a function that takes a filepath 
+as input and returns a string of text instead."""
+
+def get_book_text(filepath):
+	with open(filepath) as f:
+		file_contents = f.read()
+		return file_contents
+
+def main():
+	x = get_book_text("books/Frankenstein.txt")
+	print(x)
+
+main()
