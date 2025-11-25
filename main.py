51548dd54a15e75f8c2414e3937cc25d962e6df6
crackajacknmac <crackajacknmac@gmail.com>
2025-11-25 09:14:50 -0800

    H: working on the report function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stats import book_word_count
 from stats import character_count
-
+from stats import create_sorted_list
 
 def get_book_text(filepath):
 	with open("books/frankenstein.txt") as f:
@@ -9,9 +9,15 @@ def get_book_text(filepath):
 
 def main():
 	text = get_book_text("books/frankenstein.txt")
+	print("============ BOOKBOT ============")
+	print("Analyzing book found at books/frankenstein.txt...")
+	print("----------- Word Count ----------")
 	num_words = book_word_count(text)
 	print(f"Found {num_words} total words")
+	print("--------- Character Count -------")
 	character_counter = character_count(text)
-	print(f"{character_counter}")
+	final_result = create_sorted_list(character_counter)
+	print(f"{final_result}")
+	print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -4,7 +4,7 @@ def book_word_count(text):
         words = wc_text.split()
         for each in words:
                 word_count +=1
-        return word_count"
+        return word_count
 
 def character_count(text):
 	character_dict ={}
@@ -18,5 +18,19 @@ def character_count(text):
 
 """Don't touch above this line"""
 
-def create_sorted_list(character_dict)
+"""Okay, so let's break down the problem specifically.
 
+We need to add a function that takes the key values of character_dict (letter: qty)
+and adds them to a new dictionary.
+That dictionary needs to be then added to a list.
+All the letters need their own dictionary in the list.
+That list needs to be called and printed as output to the terminal.
+Output must be in alphabetical order and just as shown in the lesson."""
+
+def create_sorted_list(character_dict):
+	report_list = [] #Here's the list that all the dictionaries will save to.
+	for entry in character_dict: #This iterates over the key/values in the dictionary
+		if entry.isalpha(): #This triggers the if statement for only alphabetical characters
+			report_list.append(character_dict) #This adds the dictionary entry to the list
+
+	return report_list
